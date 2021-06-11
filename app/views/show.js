b9ec1396ec17014e6ed22bb2ce020557061b0889
Ryan Lee Simms <ryan_simms@apple.com>
2021-06-11 13:36:20 -0400

    initial commit

---
+++ app/views/show.js
@@ -0,0 +1,10 @@
+export default function show({ widget }) {
+  return `
+    <table class="table">
+      <tr><td>ID</td><td>${widget.id}</td></tr>
+      <tr><td>Name</td><td>${widget.name.toLowerCase()}</td></tr>
+      <tr><td>Purpose</td><td>${widget.purpose}</td></tr>
+      <tr><td>Active</td><td>${widget.active.toString()}</td></tr>
+    </table>
+  `;
+}
